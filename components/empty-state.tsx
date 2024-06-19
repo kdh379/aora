af91dcfec207d77ba5d820f381fc3d5dd9b6b00a
kdh379 <ehgus37625@naver.com>
2024-06-20 00:28:43 +0900

    refactor: empty state
    - 상호작용 버튼 제거
    - children 추가

--- components/empty-state.tsx
+++ components/empty-state.tsx
@@ -2,16 +2,16 @@ import { View, Text, Image } from "react-native";
 import React from "react";
 
 import { images } from "@/constants";
-import CustomButton from "@/components/custom-button";
 import { cn } from "@/lib/cn";
 
 interface EmptyStateProps {
   title: string;
   subtitle: string;
   className?: string;
+  children?: React.ReactNode;
 }
 
-const EmptyState = ({ title, subtitle, className }: EmptyStateProps) => {
+const EmptyState = ({ title, subtitle, children, className }: EmptyStateProps) => {
   return (
     <View className={cn("items-center justify-center px-4", className)}>
       <Image
@@ -22,9 +22,7 @@ const EmptyState = ({ title, subtitle, className }: EmptyStateProps) => {
       <Text className="mt-2 text-center font-semibold text-xl text-white">{title}</Text>
       <Text className="text-center font-medium text-sm text-gray-100">{subtitle}</Text>
 
-      <CustomButton className="my-5 w-full">
-        <Text className="font-semibold text-lg text-primary">비디오 만들기</Text>
-      </CustomButton>
+      {children}
     </View>
   );
 };
